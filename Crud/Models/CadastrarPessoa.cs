2501a89335941b149e9321aeda4ba1e6b9e20142
Gustavo Linhares <gustavo.rocco.02@gmail.com>
2020-11-23 21:47:55 -0300

    Primeiro Commit

---
+++ Crud/Models/CadastrarPessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crud.Models
+{
+    public class CadastrarPessoa
+    {
+        public int PessoaID { get; set; }
+
+        public string NomeCompleto { get; set; }
+
+        public string Email { get; set; }
+
+        public string Senha { get; set; }
+    }
+}
---
+++ Crud/Models/ContextoCadastro.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crud.Models
+{
+    public class ContextoCadastro : DbContext
+    {
+        public DbSet<CadastrarPessoa> CadastrarPessoas { get; set; }
+
+        public ContextoCadastro(DbContextOptions<ContextoCadastro> options) : base(options)
+        {
+
+        }
+    }
+}
